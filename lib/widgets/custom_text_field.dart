6776f2282f3a37e6759154f5de542cbfa6a411f9
AhmedBelal31 <ahmedbelal1.exe@gmail.com>
2024-05-10 15:46:44 +0300

    Build Custom TextFormField

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String hint;
+  final TextEditingController? controller;
+  final void Function(String)? onFieldSubmitted;
+  final void Function(String)? onChanged;
+  final bool obscureText;
+
+  const CustomTextFormField({
+    super.key,
+    required this.hint,
+    this.controller,
+    this.onFieldSubmitted,
+    this.onChanged,
+    this.obscureText = false ,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      onFieldSubmitted: onFieldSubmitted,
+      onChanged: onChanged,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        hintText: hint,
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: const BorderSide(color: Colors.blue, width: 2)),
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: const BorderSide(color: Colors.orange, width: 1)),
+      ),
+    );
+  }
+}
