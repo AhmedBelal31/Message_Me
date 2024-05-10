53903bbe04ee450b2317f51a141e764fd060e80a
AhmedBelal31 <ahmedbelal1.exe@gmail.com>
2024-05-10 16:04:47 +0300

    Registration Screen Done

---
+++ lib/screens/registration_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:message_me/widgets/custom_button.dart';
+import '../widgets/custom_text_field.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({super.key});
+
+  @override
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(
+              height: 180,
+              child: Image.asset('images/logo.png'),
+            ),
+            const SizedBox(height: 50),
+            // ignore: prefer_const_constructors
+            CustomTextFormField(hint: "Enter your Email "),
+            const SizedBox(height: 8),
+            CustomTextFormField(
+              hint: "Enter your Password ",
+              obscureText: true,
+            ),
+              const SizedBox(height: 10),
+            CustomButton(
+              title: 'Register',
+              backgroundColor: Colors.blue[800]!,
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
