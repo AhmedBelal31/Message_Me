fa11beec9b7c1777d3c0a77cdfebe9883567105e
AhmedBelal31 <ahmedbelal1.exe@gmail.com>
2024-05-10 16:05:14 +0300

    Create Sign In Screen

---
+++ lib/screens/sign_in_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:message_me/widgets/custom_button.dart';
+import '../widgets/custom_text_field.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(
+              height: 180,
+              child: Image.asset('images/logo.png'),
+            ),
+            const SizedBox(height: 50),
+            // ignore: prefer_const_constructors
+            CustomTextFormField(hint: "Enter your Email "),
+            const SizedBox(height: 8),
+            CustomTextFormField(
+              hint: "Enter your Password ",
+              obscureText: true,
+            ),
+              const SizedBox(height: 10),
+            CustomButton(
+              title: 'Sign In',
+              backgroundColor: Colors.yellow[900]!,
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
