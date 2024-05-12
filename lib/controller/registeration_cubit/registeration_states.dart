85e1d9183622c59fc8b2d561f0b2fa54cbbdf67a
AhmedBelal31 <ahmedbelal1.exe@gmail.com>
2024-05-12 14:25:15 +0300

    Create SignOut States

--- lib/controller/registeration_cubit/registeration_states.dart
+++ lib/controller/registeration_cubit/registeration_states.dart
@@ -13,8 +13,6 @@ class RegisterationFailureState extends RegisterationStates {
 }
 
 // Login States 
-
-
 class LoginLoadingState extends RegisterationStates {}
 
 class LoginSuccessfulState extends RegisterationStates {}
@@ -22,4 +20,14 @@ class LoginSuccessfulState extends RegisterationStates {}
 class LoginFailureState extends RegisterationStates {
   dynamic errorMessage;
   LoginFailureState({required this.errorMessage});
+}
+
+
+class SignoutLoadingState extends RegisterationStates {}
+
+class SignoutSuccessfulState extends RegisterationStates {}
+
+class SignoutFailureState extends RegisterationStates {
+  dynamic errorMessage;
+  SignoutFailureState({required this.errorMessage});
 }
