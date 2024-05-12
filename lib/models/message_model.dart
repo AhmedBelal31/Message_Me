d3c8d0784d29e66374e3c27ed0709e5b50bd5175
AhmedBelal31 <ahmedbelal1.exe@gmail.com>
2024-05-12 16:33:16 +0300

    Create Message Model

---
+++ lib/models/message_model.dart
@@ -0,0 +1,28 @@
+class MessageModel {
+  String message;
+  DateTime dateTime;
+  String email;
+
+  MessageModel({
+    required this.message,
+    required this.email,
+    required this.dateTime,
+  });
+
+  factory MessageModel.fromJson(Map<String, dynamic> json) {
+    return MessageModel(
+      message: json['message'],
+      email: json['email'],
+      dateTime: json['dateTime'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'message' :message ,
+      'email' :email ,
+      'dateTime' :dateTime ,
+
+    };
+  }
+}
