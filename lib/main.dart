d7f8eff8ba79f3764ed7e04cb5afe51ce771e2a9
AhmedBelal31 <ahmedbelal1.exe@gmail.com>
2024-05-16 16:07:02 +0300

    Add bloc Observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:message_me/screens/chat_screen.dart';
 import 'package:message_me/screens/registration_screen.dart';
 import 'package:message_me/screens/sign_in_screen.dart';
 import 'package:message_me/screens/welcome_screen.dart';
 // Import the generated file
 import 'firebase_options.dart';
+import 'helper/bloc_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -25,24 +28,20 @@ class MyApp extends StatelessWidget {
       title: 'Message Me',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(
-          iconTheme: IconThemeData(
-            color: Colors.white
-          )
-        )
-      ),
+          // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          scaffoldBackgroundColor: Colors.white,
+          appBarTheme: const AppBarTheme(
+            iconTheme: IconThemeData(color: Colors.white),
+          )),
+      // home : ChatScreen(),
       initialRoute: WelcomeScreen.screenRoute,
       routes: {
-        WelcomeScreen.screenRoute : (context)=>const WelcomeScreen(),
-        SignInScreen.screenRoute : (context)=>const SignInScreen(),
-        RegistrationScreen.screenRoute : (context)=>const RegistrationScreen(),
-        ChatScreen.screenRoute : (context)=>const ChatScreen(),
-        
+        WelcomeScreen.screenRoute: (context) => const WelcomeScreen(),
+        SignInScreen.screenRoute: (context) => const SignInScreen(),
+        RegistrationScreen.screenRoute: (context) => const RegistrationScreen(),
+        ChatScreen.screenRoute: (context) => const ChatScreen(),
       },
-
     );
   }
 }
